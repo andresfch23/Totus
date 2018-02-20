d47bb3130bf78240f1262508a36d98e1edcf6593
Andres Felipe Chavez Herrera <felipe.chavez@udea.edu.co>
2018-02-19 21:31:32 -0500

    creating the header component

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from '../images/nombre.png';
+
+class Header extends Component {
+    render() {
+        return(
+            <header className="header">
+                <div className="header__social">
+                    <div className="header__icon"></div>
+                    <div className="header__icon"></div> 
+                    <div className="header__icon"></div>
+                    <div className="header__icon"></div>
+                </div>
+
+                <div className="header__caption">
+                    <p className="header__text">"Mi pasado, Señor, lo confío a tu misericordia, mi presente a tu amor, mi futuro a tu providencia.” San Pío de Pietrecina</p>
+                </div>
+
+                <img className="header__image" src={logo} alt="user face"></img>
+
+            </header>
+        );
+    }
+}
+
+export default Header;
