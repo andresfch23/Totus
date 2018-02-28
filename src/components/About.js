660f303349a7d52527931b800bc707b47cf3ce27
Andres Felipe Chavez Herrera <felipe.chavez@udea.edu.co>
2018-02-28 18:38:23 -0500

    finalize the 'About Us' component

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import ImgStatueMary from '../assets/images/4.jpg';
+import ProfileImage from '../assets/images/Jhoana.jpg'
 
 class About extends Component {
   render() {
@@ -15,7 +15,8 @@ class About extends Component {
                   llevar el amor de Dios a muchas personas. Se creó
                   en Colombia en la ciudad de Barranquilla en el año
                   2014 y su distribución es totalmente GRATIS.
-                  Inicialmente se publicó cada dos meses de manera
+                  </p>
+                  <p className="about__description-content">Inicialmente se publicó cada dos meses de manera
                   impresa pero apartir del 2018 la revista es via
                   electronica.</p>
                   <p>Contamos con la bendición de la Arquidiócesis de
@@ -24,25 +25,28 @@ class About extends Component {
               <div className="about__description-image">
               </div>
           </div>
-          <div className="grid__item three-fifths">
+          <div className="grid__item three-fifths about__biography">
               <div className="about__presentation">
-                  <div className="about__">
+                  <div className="about__profile">
+                        <img className="about__profile-image" src={ProfileImage} alt="Profile"></img>
+                        <span className="about__profile-name">Jhovanna</span>
                   </div>
-                  <p>Mi nombre es Jhovanna Rodríguez Rojas, soy Diseñadora
-                  Gráfica de profesión de la Universidad del Norte y misionera
-                  de la comunidad Lazos de Amor Mariano desde hace
-                  aproximadamente 7 años. Nací en la ciudad de Bogotá el 31
-                  de enero de 1991.
-                  Durante seis meses del año 2014, viví una experiencia de
-                  servicio en la cual me dediqué totalmente a Dios y a la
-                  evangelización a través de la Comunidad Lazos de Amor
-                  Mariano; gracias a una misión que desarrollé en el
-                  Departamento de Córdoba, nació en mi corazón la idea de
-                  crear una revista católica que me permitiera utilizar mis
-                  conocimientos y habilidades para la evangelización.</p>
+                  <div className="about__presentation-text">
+                        <p className="about__presentation-text-first">Mi nombre es Jhovanna Rodríguez Rojas, soy Diseñadora
+                        Gráfica de profesión de la Universidad del Norte y misionera
+                        de la comunidad Lazos de Amor Mariano desde hace
+                        aproximadamente 7 años. Nací en la ciudad de Bogotá el 31
+                        de enero de 1991.</p>
+                        <p className="about__presentation-text-first">Durante seis meses del año 2014, viví una experiencia de
+                        servicio en la cual me dediqué totalmente a Dios y a la
+                        evangelización a través de la Comunidad Lazos de Amor
+                        Mariano; gracias a una misión que desarrollé en el
+                        Departamento de Córdoba, nació en mi corazón la idea de
+                        crear una revista católica que me permitiera utilizar mis
+                        conocimientos y habilidades para la evangelización.</p>
+                  </div>
               </div>
-              <div>
-                <img src={ImgStatueMary} alt="Statue of Mary"></img>
+              <div className="about__image">
               </div>
           </div>
       </div>
